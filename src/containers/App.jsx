45f110a9e2808281470b6f42d130395ece87bc76
Cesar <cmendi@MacBook-Pro-de-C.local>
2019-10-08 21:48:22 -0500

    Carousel listo

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import Carouselitem from '../components/Carouselitem';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
+    <Search />
+    <Categories>
+      <Carousel>
+        <Carouselitem />
+        <Carouselitem />
+        <Carouselitem />
+        <Carouselitem />
+      </Carousel>
+    </Categories>
   </div>
 );
 
