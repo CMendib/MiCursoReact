2ef6b2b00ce536d5a0a0b16414f9f7fb6166b0de
Cesar <cmendi@MacBook-Pro-de-C.local>
2019-10-09 21:28:09 -0500

    Antes de ApI

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,13 +4,15 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import Carouselitem from '../components/Carouselitem';
+import Footer from '../components/Footer';
+
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+    <Categories title="Mi lista">
       <Carousel>
         <Carouselitem />
         <Carouselitem />
@@ -18,6 +20,20 @@ const App = () => (
         <Carouselitem />
       </Carousel>
     </Categories>
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <Carouselitem />
+        <Carouselitem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales de Pltzi Video">
+      <Carousel>
+        <Carouselitem />
+      </Carousel>
+    </Categories>
+    <Footer />
   </div>
 );
 
